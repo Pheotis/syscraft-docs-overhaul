90821dacbae311f1552dfdbf7a750cd31b44b771
Pheotis <49039536+Pheotis@users.noreply.github.com>
2024-10-15 12:56:03 -0400

    Attempt to add collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,42 @@
 import { defineCollection, z } from 'astro:content';
 import { SITE } from '../consts';
 
-const docs = defineCollection({
-	schema: z.object({
-		title: z.string().default(SITE.title),
-		description: z.string().default(SITE.description),
-		lang: z.literal('en-us').default(SITE.defaultLanguage),
-		dir: z.union([z.literal('ltr'), z.literal('rtl')]).default('ltr'),
-		image: z
-			.object({
-				src: z.string(),
-				alt: z.string(),
-			})
-			.optional(),
-		ogLocale: z.string().optional(),
-	}),
+export const collections = { docs };
+
+const admin = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+const dev = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+const host = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+const mod = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
 });
 
-export const collections = { docs };
+export const collections = { admin, dev, host, mod };
+
+
+config.ts
